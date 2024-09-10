e588da049cdecc6b4d3f461941723a38b7eeac7a
namuan <575441+namuan@users.noreply.github.com>
2024-09-10 14:25:40 +0100

    feat: Update design and other functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, redirect, url_for
+from flask import Flask, render_template, jsonify, redirect, url_for, abort
 import json
 import ast
 import csv
@@ -21,13 +21,36 @@ def load_products():
 products = load_products()
 current_index = 0
 
+def get_product_by_id(product_id):
+    try:
+        product_id = int(product_id)
+        if 1 <= product_id <= len(products):
+            return products[product_id - 1], product_id - 1
+    except ValueError:
+        pass
+    return None, None
+
 @app.route('/')
 def show_product():
     global current_index
     product = products[current_index]
     # Format the release date to remove the timestamp
     product['release_date'] = product['release_date'].split(' ')[0]
+    # Add the product ID to the context
+    product['id'] = current_index + 1
     return render_template('product.html', product=product)
+
+@app.route('/product/<product_id>')
+def show_product_by_id(product_id):
+    global current_index
+    product, index = get_product_by_id(product_id)
+    if product:
+        current_index = index
+        product['release_date'] = product['release_date'].split(' ')[0]
+        product['id'] = index + 1
+        return render_template('product.html', product=product)
+    else:
+        abort(404)
 
 @app.route('/like', methods=['POST'])
 def like_product():
@@ -44,18 +67,18 @@ def like_product():
         with open('liked_products.json', 'w', encoding='utf-8') as file:
             json.dump([product], file, ensure_ascii=False, indent=4)
     return jsonify(success=True, redirect=url_for('next_product'))
+
 @app.route('/next')
 def next_product():
     global current_index
     current_index = (current_index + 1) % len(products)
-    return show_product()
+    return redirect(url_for('show_product_by_id', product_id=current_index + 1))
 
 @app.route('/previous')
 def previous_product():
     global current_index
     current_index = (current_index - 1) % len(products)
-    return show_product()
-
+    return redirect(url_for('show_product_by_id', product_id=current_index + 1))
 
 if __name__ == '__main__':
     app.run(port=5011, debug=True)
